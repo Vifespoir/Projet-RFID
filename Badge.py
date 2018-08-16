1fdff4daa9a5413dbf7a074b6700632f3a45270a
Etienne <etienne.pouget@outlook.com>
2018-08-16 16:23:07 +0200

    fixed formatting

--- Badge.py
+++ Badge.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python3
 # Version modifiee de la librairie https://github.com/mxgxw/MFRC522-python
-
-# import RPi.GPIO as GPIO
-# FOR ORANGEPI ONLY
-from pyA20.gpio import gpio
-from pyA20.gpio import port
-from pyA20.gpio import connector
-
-gpio.init() #Initialize module. Always called first
+import signal
+from datetime import strftime, strptime
+from time import sleep
 
 import MFRC522
-import signal
-import time
-import datetime
-from datetime import date
-from datetime import datetime
+# import RPi.GPIO as GPIO
+# FOR ORANGEPI ONLY
+# from pyA20.gpio import port
+# from pyA20.gpio import connector
+from pyA20.gpio import gpio
+
+gpio.init()  # Initialize module. Always called first
+
 
 fichier = '/home/michel/Documents/test.csv'
 sortie = '/home/michel/Documents/sortie.txt'
@@ -22,26 +20,28 @@ no_adherent = '/home/michel/Documents/non_repertorie.txt'
 
 continue_reading = True
 
-# Fonction qui arrete la lecture proprement 
-def end_read(signal,frame):
+
+def end_read(signal, frame):
+    """Fonction qui arrete la lecture proprement."""
     global continue_reading
     print ("Lecture terminée")
     continue_reading = False
     gpio.cleanup()
 
-def faire_string(ligne):
-        ligne = line.split(',')
-        date_cotisation = datetime.strptime(ligne[4],'%d/%m/%Y')
-        date = date_cotisation.date()
-        difference = date.today() - date
-        if difference.days <365:
-            texte = ' Oui'
-        else : texte = ' Date_de_cotisation_depassee'
-        date = time.strftime("%A %d %B %Y %H:%M")
-        return '\n' +  date +' '+ ligne[2] + ' ' + ligne[1] + texte
-        
 
-    
+def faire_string(ligne):
+    ligne = line.split(',')
+    date_cotisation = strptime(ligne[4], '%d/%m/%Y')
+    date = date_cotisation.date()
+    difference = date.today() - date
+    if difference.days < 365:
+        texte = ' Oui'
+    else:
+        texte = ' Date_de_cotisation_depassee'
+    date = strftime("%A %d %B %Y %H:%M")
+
+    return '\n' + date + ' ' + ligne[2] + ' ' + ligne[1] + texte
+
 
 print("init")
 signal.signal(signal.SIGINT, end_read)
@@ -50,21 +50,20 @@ MIFAREReader = MFRC522.MFRC522()
 print ("Passer le tag RFID a lire")
 
 while continue_reading:
-    
     # Detecter les tags
-    (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+    (status, TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
 
     # Une carte est detectee
     if status == MIFAREReader.MI_OK:
         print ("Carte detectee")
-    
+
     # Recuperation UID
-    (status,uid) = MIFAREReader.MFRC522_Anticoll()
+    (status, uid) = MIFAREReader.MFRC522_Anticoll()
     if status == MIFAREReader.MI_OK:
 
         # Clee d authentification par defaut
-        key = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
-        
+        key = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
+
         # Selection du tag
         MIFAREReader.MFRC522_SelectTag(uid)
 
@@ -73,24 +72,23 @@ while continue_reading:
 
         code = str(uid[0])+str(uid[1])+str(uid[2])+str(uid[3])
 
-        compteur=0
-        for line in open (fichier):
+        compteur = 0
+        for line in open(fichier):
             if code in line:
-                compteur+=1
-                test = open(sortie,'a')
+                compteur += 1
+                test = open(sortie, 'a')
                 entree = faire_string(line)
                 test.write(entree)
                 test.close()
 
         if compteur == 0:
-            with open(no_adherent,'w') as no_adhe:
+            with open(no_adherent, 'w') as no_adhe:
                 no_adhe.write(code)
                 print("carte non repertioriee")
-
 
         if status == MIFAREReader.MI_OK:
             MIFAREReader.MFRC522_StopCrypto1()
         else:
             print ("Erreur d\'Authentification")
-            
-        time.sleep(3)
+
+        sleep(3)
--- Ecriture.py
+++ Ecriture.py
@@ -2,51 +2,56 @@
 # -*- coding: utf8 -*-
 # Version modifiee de la librairie https://github.com/mxgxw/MFRC522-python
 
-import RPi.GPIO as GPIO
-import MFRC522
 import signal
+
+import MFRC522
+import RPi.GPIO as GPIO
 
 continue_reading = True
 
-# Fonction qui arrete la lecture proprement 
-def end_read(signal,frame):
+
+def end_read(signal, frame):
+    """Fonction qui arrete la lecture proprement."""
     global continue_reading
     print ("Lecture terminée")
     continue_reading = False
     GPIO.cleanup()
+
 
 signal.signal(signal.SIGINT, end_read)
 MIFAREReader = MFRC522.MFRC522()
 
 data = []
 texte = raw_input("Entrez une chaine de caractère :\n")
+
 for c in texte:
-    if (len(data)<16):
+    if (len(data) < 16):
         data.append(int(ord(c)))
-while(len(data)!=16):
+
+while(len(data) != 16):
     data.append(0)
+
 print ("Placez votre carte RFID")
 
 while continue_reading:
-      
     # Detecter les tags
-    (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+    (status, TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
 
     # Une carte est detectee
     if status == MIFAREReader.MI_OK:
         print ("Carte detectee")
-    
+
     # Recuperation UID
-    (status,uid) = MIFAREReader.MFRC522_Anticoll()
+    (status, uid) = MIFAREReader.MFRC522_Anticoll()
 
     if status == MIFAREReader.MI_OK:
 
         # Print UID
         print ("UID de la carte : "+str(uid[0])+"."+str(uid[1])+"."+str(uid[2])+"."+str(uid[3]))
-    
+
         # Clee d authentification par defaut
-        key = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
-        
+        key = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
+
         # Selection du tag
         MIFAREReader.MFRC522_SelectTag(uid)
 
