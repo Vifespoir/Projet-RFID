6d0bd439fa55179893ca4d3c2a2ecd41cb119a2b
Etienne Pouget <etienne.pouget@outlook.com>
2018-08-25 17:25:24 +0200

    corrected test

--- modules/entree_sortie.py
+++ modules/entree_sortie.py
@@ -206,10 +206,15 @@ def test_fichier_csv(fichier):
                 assert TEST_MME_MR.match(ligne[0]), "Problem with prefix :" + ligne[0]
                 assert TEST_NOM.match(ligne[1]), "Problem with 1st name :" + ligne[1]
                 assert TEST_NOM.match(ligne[2]), "Problem with last name :" + ligne[2]
-                assert TEST_EMAIL.match(ligne[3]), "Problem with email :" + ligne[3]
+                if ligne[3]:
+                    assert TEST_EMAIL.match(ligne[3]), "Problem with email :" + ligne[3]
+                else:
+                    print("ERREUR SUR LA LIGNE: {}".format(ligne))
                 assert TEST_DATE.match(ligne[4]), "Problem with date :" + ligne[4]
                 if ligne[5]:
                     assert TEST_RFID.match(ligne[5]), "Problem with RFID :" + ligne[5]
+                else:
+                    print("ERREUR NO RFID")
             except AssertionError as e:
                 print(e)
                 return "Erreur ligne: {}\n{}\n{}".format(compteur, ", ".join(ligne), e)
